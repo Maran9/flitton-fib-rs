ec1d8ae63a8a29abe3048ca7513948419bdda737
Maran9 <87171972+Maran9@users.noreply.github.com>
2021-10-27 10:35:28 +0530

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,24 @@ use interface::object::__pyo3_get_function_object_interface;
 
 
 #[pyfunction]
+fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
+
+    let mut buffer: Vec<i32> = Vec::new();
+    let first_vector: Vec<i32> = (0..total_vector_size.clone()
+                                        ).map(|x| x).collect();
+    let second_vector: Vec<i32> = (0..total_vector_size
+                                        ).map(|x| x).collect();
+
+    for i in &first_vector {
+        buffer.push(first_vector[**&i as usize] +
+                     second_vector[*i as usize]);
+    }
+   return buffer
+}
+
+
+
+#[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
 }
@@ -23,5 +41,6 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_wrapped(wrap_pyfunction!(time_add_vectors));
     Ok(())
 }
